5ec30cbce8d49d8b48c0f87414ce4a3e50d2fe70
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-11-24 20:42:12 +0100

    Update 04_clever_lily.py

--- PROGRAMMING BASICS WITH PYTHON-FEB 2023/L12_while_loops_exercise/04_clever_lily.py
+++ PROGRAMMING BASICS WITH PYTHON-FEB 2023/L12_while_loops_exercise/04_clever_lily.py
@@ -1,14 +1,12 @@
-# Read input
+
 age = int(input())
 washing_machine_price = float(input())
 toy_price = int(input())
 
-# Initialize variables
 total_money = 0
 money_gift = 10  # Starting money gift for even years
 toys_count = 0
 
-# Loop through each year up to the current age
 for i in range(1, age + 1):
     if i % 2 == 0:  # Even year, she receives money
         total_money += money_gift  # Add money gift to total
@@ -17,10 +15,8 @@ for i in range(1, age + 1):
     else:  # Odd year, she receives a toy
         toys_count += 1
 
-# Add money from selling toys
 total_money += toys_count * toy_price
 
-# Check if she can buy the washing machine
 if total_money >= washing_machine_price:
     print(f"Yes! {total_money - washing_machine_price:.2f}")
 else:
