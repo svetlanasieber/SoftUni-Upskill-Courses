ec77e9384ba8e1d656af1011def17973f42af246
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-11-24 20:40:40 +0100

    Update 03_sum_numbers.py

--- PROGRAMMING BASICS WITH PYTHON-FEB 2023/L11_while_loops_lab/03_sum_numbers.py
+++ PROGRAMMING BASICS WITH PYTHON-FEB 2023/L11_while_loops_lab/03_sum_numbers.py
@@ -1,6 +1,6 @@
 control_number = int(input())
 sum_of_numbers = 0
-# Докато сумата на числата е по-малка на контролното число
+
 while sum_of_numbers < control_number:
     current_number = int(input())
     sum_of_numbers += current_number
