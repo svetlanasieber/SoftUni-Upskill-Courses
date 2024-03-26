673eba25e17d9c92a0710c7c4229b7f4ad336dc7
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 12:51:27 +0100

    Add files via upload

---
+++ PROGRAMMING BASICS WITH CSharp - JAN 2024/Exam-23-03-2024/task5_BestPlayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string command = Console.ReadLine();
+
+        string bestPlayer = "";
+        int bestScore = 0;
+
+        while (command != "END")
+        {
+            string playerName = command;
+            int count_of_goals = int.Parse(Console.ReadLine());
+
+            if (count_of_goals > bestScore)
+            {
+                bestScore = count_of_goals;
+                bestPlayer = playerName;
+            }
+
+            if (count_of_goals >= 10)
+            {
+                break;
+            }
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine($"{bestPlayer} is the best player!");
+
+        if (bestScore >= 3)
+        {
+            Console.WriteLine($"He has scored {bestScore} goals and made a hat-trick !!!");
+        }
+        else
+        {
+            Console.WriteLine($"He has scored {bestScore} goals.");
+        }
+    }
+}
+
+
