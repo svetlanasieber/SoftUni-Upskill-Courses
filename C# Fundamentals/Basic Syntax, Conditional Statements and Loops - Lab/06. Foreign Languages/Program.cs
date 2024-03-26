8868b23134424bc8f3d464bd8ed20985213aafa9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 22:26:49 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/06. Foreign Languages/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/06. Foreign Languages/Program.cs
@@ -1,12 +1,3 @@
-﻿//Create a program that prints the spoken language in a country. You will receive only the following combinations: 
-//•	English is spoken in England and the USA.
-//•	Spanish is spoken in Spain, Argentina, and Mexico.
-//•	For the others, we should print "unknown".
-//Input
-//You will receive a single line of input, representing the country name.
-//Output
-//Print the language spoken in the given country. If the country is unknown for the program, print "unknown".
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
