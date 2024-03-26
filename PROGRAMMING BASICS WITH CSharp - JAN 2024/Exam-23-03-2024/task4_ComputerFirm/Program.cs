5bce434006b9f8246ced95848e2d4eaea3af6b47
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 12:50:34 +0100

    Add files via upload

---
+++ PROGRAMMING BASICS WITH CSharp - JAN 2024/Exam-23-03-2024/task4_ComputerFirm/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double totalSales = 0;
+        double totalRating = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int computerData = int.Parse(Console.ReadLine());
+            int sales = computerData / 10;
+            int rating = computerData % 10;
+
+            switch (rating)
+            {
+                case 2:
+                    totalSales += sales * 0.0;
+                    break;
+                case 3:
+                    totalSales += sales * 0.5;
+                    break;
+                case 4:
+                    totalSales += sales * 0.7;
+                    break;
+                case 5:
+                    totalSales += sales * 0.85;
+                    break;
+                case 6:
+                    totalSales += sales * 1.0;
+                    break;
+            }
+
+            totalRating += rating;
+        }
+
+        double averageRating = totalRating / n;
+
+        Console.WriteLine($"{totalSales:F2}");
+        Console.WriteLine($"{averageRating:F2}");
+    }
+}
+
+
