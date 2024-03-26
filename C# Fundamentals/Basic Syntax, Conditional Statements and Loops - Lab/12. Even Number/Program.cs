b00cdf1f8fe27b4044fc36a33939174c8679d76f
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 22:35:34 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/12. Even Number/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/12. Even Number/Program.cs
@@ -1,8 +1,3 @@
-﻿//Create a program that reads a sequence of numbers.
-//If the number is even, print its absolute value in the following format:
-//"The number is: {number}", and terminate the program.
-//If the number is odd, print "Please write an even number." and continue reading numbers.
-
 using System;
 
 
