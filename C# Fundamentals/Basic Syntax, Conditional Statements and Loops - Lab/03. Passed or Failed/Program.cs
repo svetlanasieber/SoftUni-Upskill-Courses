8aa7bbe2d882334b8762fbf36d05be9817e94e2a
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 22:21:44 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/03. Passed or Failed/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/03. Passed or Failed/Program.cs
@@ -1,9 +1,3 @@
-﻿//Modify the program from the previous problem, so it will print "Failed!" if the grade is lower than 3.00.
-//Input
-//The input comes as a single double number.
-//Output
-//The output is either "Passed!" if the grade is more than 2.99, otherwise you should print "Failed!".
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
