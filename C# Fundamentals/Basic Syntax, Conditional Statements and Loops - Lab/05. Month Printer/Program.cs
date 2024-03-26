06995eacb66432022525774f2fb8f8e638afc4de
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 22:24:40 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/05. Month Printer/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/05. Month Printer/Program.cs
@@ -1,10 +1,3 @@
-﻿//Create a program that receives an integer and prints the matching month.
-//If the number is more than 12 or less than 1 print "Error!".
-//Input
-//You will receive a single integer on a single line.
-//Output
-//If the number is within the boundaries print the corresponding month, otherwise print "Error!".
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
