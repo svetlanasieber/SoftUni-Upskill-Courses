f5f6dfcda3f9ec9b84028126d1ed345046477fbb
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 22:28:30 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/Program.cs
@@ -1,16 +1,3 @@
-﻿//A theatre is sail tickets at discount, and a program is needed to calculate the price of a single ticket.
-//If the given age does not fit one of the categories, you should print "Error!".  
-//The prices of the tickers are as follows:
-//Day / Age   0 <= age <= 18  18 < age <= 64  64 < age <= 122
-//Weekday 12$	18$	12$
-//Weekend 15$	20$	15$
-//Holiday 5$	12$	10$
-//Input
-//The input comes in two lines. On the first line, you will receive the type of day.
-//On the second – the age of the person.
-//Output
-//Print the price of the ticket according to the table, or "Error!" if the age is not in the table.
-
 using System;
 
 
