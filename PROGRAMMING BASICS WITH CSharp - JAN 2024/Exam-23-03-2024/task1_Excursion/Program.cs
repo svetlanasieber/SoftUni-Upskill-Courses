17801896a78934938b3ee5d1a6888e0a399ffa25
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 12:47:07 +0100

    Add files via upload

---
+++ PROGRAMMING BASICS WITH CSharp - JAN 2024/Exam-23-03-2024/task1_Excursion/Program.cs
@@ -0,0 +1,16 @@
+﻿int groupSize = int.Parse(Console.ReadLine());
+int nights = int.Parse(Console.ReadLine());
+int transportCards = int.Parse(Console.ReadLine());
+int museumTickets = int.Parse(Console.ReadLine());
+
+const double NIGHT_COST = 20.0;
+const double TRANSPORT_COST = 1.60;
+const double MUSEUM_COST = 6.0;
+
+double personTotal = (nights * NIGHT_COST) + (transportCards * TRANSPORT_COST) + (museumTickets * TRANSPORT_COST);
+
+double groupTotal = personTotal * groupSize;
+double total = groupTotal + (groupTotal * 0.25);
+
+Console.WriteLine($"{total:F2}");
+
