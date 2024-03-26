191c954668dd08494ffbff656ec61d54e34f06cf
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-26 22:37:12 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/13. Holidays Between Two Dates/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/13. Holidays Between Two Dates/Program.cs
@@ -1,8 +1,3 @@
-﻿//You are assigned to find and fix all bugs in the existing code.
-//By using the Visual Studio debugger, place a breakpoint and find the lines of code that produce incorrect or unexpected results.
-//You are given a program (existing source code) that aims to count the non-working days between two dates in format day.month.year
-//(e.g. between 1.05.2015 and 15.05.2015 there are 5 non-working days – Saturday and Sunday).
-
 using System;
 using System.Globalization;
 
