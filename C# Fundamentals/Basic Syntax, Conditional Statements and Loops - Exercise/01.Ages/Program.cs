d66d070eaf2399bc2da3a1147a0538db3953fc03
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-03-29 23:14:00 +0100

    Update Program.cs

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/01.Ages/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/01.Ages/Program.cs
@@ -1,11 +1,3 @@
-﻿//Write a program that determines if a person is baby, child, teenager, adult, elder based on the given age. The bounders are:
-//•	0 - 2 – baby
-//•	3 - 13 – child
-//•	14 - 19 – teenager
-//•	20 - 65 – adult
-//•	>= 66 – elder
-//•	All the values are inclusive.
-
 using System;
 
 
