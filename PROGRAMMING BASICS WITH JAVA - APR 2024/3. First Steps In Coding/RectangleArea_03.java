422db42be2a1c0af7c4ec9f6222623394d6f892e
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-11 03:10:38 +0200

    Add files via upload

---
+++ PROGRAMMING BASICS WITH JAVA - APR 2024/3. First Steps In Coding/RectangleArea_03.java
@@ -0,0 +1,15 @@
+package FirstStepsInCodingLab;
+
+import java.util.Scanner;
+
+public class RectangleArea_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sideA = Integer.parseInt(scanner.nextLine());
+        int sideB = Integer.parseInt(scanner.nextLine());
+
+        int area = sideA * sideB;
+        System.out.println(area);
+
+    }
+}
