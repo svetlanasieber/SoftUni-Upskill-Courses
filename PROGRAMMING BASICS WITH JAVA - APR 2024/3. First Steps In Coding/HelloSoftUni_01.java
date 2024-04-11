e758f37271405bb87406a4def78e2d033b6fc732
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-11 02:59:51 +0200

    Add files via upload

---
+++ PROGRAMMING BASICS WITH JAVA - APR 2024/3. First Steps In Coding/HelloSoftUni_01.java
@@ -0,0 +1,10 @@
+package FirstStepsInCodingLab;
+
+import java.util.Scanner;
+
+public class HelloSoftUni_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello SoftUni");
+    }
+}
