7cb4c8d003d51d6d35e0a81c30f4c063bab58028
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-07 00:05:04 +0200

    Add files via upload

---
+++ JAVA BASICS - April 2024/02. First Steps In Coding - Exercise/USDToBGN_01.java
@@ -0,0 +1,18 @@
+package FirstStepsInCodingExercise;
+
+
+import java.util.Scanner;
+
+public class USDToBGN_01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //1 USD = 1.79549 BGN
+        double usd = Double.parseDouble(scanner.nextLine());
+        double bgn = usd * 1.79549;
+
+        System.out.println(bgn);
+
+    }
+}
