0be9f3afaa41237039802998865352c97a3552d0
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-06 21:16:57 +0200

    Add files via upload

---
+++ JAVA BASICS - April 2024/07. For-Loop/Demo2.java
@@ -0,0 +1,16 @@
+package ForLoopLab;
+
+import java.util.Scanner;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = number; i >= 1 ; i--) {
+            System.out.println(i);
+
+        }
+    }
+}
