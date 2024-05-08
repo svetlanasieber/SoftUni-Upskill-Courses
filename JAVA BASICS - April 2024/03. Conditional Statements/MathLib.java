64ec5e416ab7ffa83cadd74e07a51827f9bf77aa
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-08 17:06:59 +0200

    Add files via upload

---
+++ JAVA BASICS - April 2024/03. Conditional Statements/MathLib.java
@@ -0,0 +1,13 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class MathLib {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(Math.ceil(23.0)); //up
+        System.out.println(Math.floor(29.9)); //down
+        System.out.println(Math.abs(-5)); // absolute value
+    }
+}
