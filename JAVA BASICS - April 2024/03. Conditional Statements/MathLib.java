ad7d29672bd656f11d9d6078147b3cf103f1361a
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-08 17:13:10 +0200

    Add files via upload

--- JAVA BASICS - April 2024/03. Conditional Statements/MathLib.java
+++ JAVA BASICS - April 2024/03. Conditional Statements/MathLib.java
@@ -9,5 +9,8 @@ public class MathLib {
         System.out.println(Math.ceil(23.0)); //up
         System.out.println(Math.floor(29.9)); //down
         System.out.println(Math.abs(-5)); // absolute value
+        System.out.println(Math.pow(2, 19)); // povdigane na stepen
+        System.out.println(Math.PI); // PI 3,14
+        System.out.println(Math.round(34.6)); // banker zakrugliane
     }
 }
