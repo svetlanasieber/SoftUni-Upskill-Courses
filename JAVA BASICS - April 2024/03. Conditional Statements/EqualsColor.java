829c254afdfb6e771ef1bca9f48b20fa0ce029f5
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-08 16:37:00 +0200

    Add files via upload

---
+++ JAVA BASICS - April 2024/03. Conditional Statements/EqualsColor.java
@@ -0,0 +1,21 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class EqualsColor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String color = "red";
+        if (color.equals("red")) {
+            System.out.println("tomato");
+        } else {
+            System.out.println("banana");
+            System.out.println("bye");
+        }
+
+
+
+    }
+    }
+
