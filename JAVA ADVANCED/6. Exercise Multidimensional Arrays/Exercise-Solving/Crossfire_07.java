f9b7fecbf3b5cce2d01a9bc8c3f77f0d9462f168
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-14 13:58:50 +0200

    Update Crossfire_07.java

--- JAVA ADVANCED/6. Exercise Multidimensional Arrays/Exercise-Solving/Crossfire_07.java
+++ JAVA ADVANCED/6. Exercise Multidimensional Arrays/Exercise-Solving/Crossfire_07.java
@@ -1,12 +1,10 @@
-package _02_MultidimensionalArrays._02_Exercise;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class _07_Crossfire {
+public class Crossfire_07 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
