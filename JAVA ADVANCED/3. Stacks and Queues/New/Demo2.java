8aae602d2370139fd170c9a03d79814db764e267
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-14 17:05:17 +0200

    Add files via upload

--- JAVA ADVANCED/3. Stacks and Queues/New/Demo2.java
+++ JAVA ADVANCED/3. Stacks and Queues/New/Demo2.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 public class Demo2 {
     public static void main(String[] args) {
-        // create a stack 
+        // create a stack
         Stack<Integer> stack = new Stack<>();
 
         // push elements onto the stack
@@ -35,3 +35,7 @@ public class Demo2 {
         System.out.println();
     }
 }
+//In this example, we create a stack and push three elements onto it.
+// We then pop the elements from the stack and print them in reverse order (3, 2, 1).
+// We also create a queue and add three elements to it.
+// We then remove the elements from the queue and print them in the order they were added (1, 2, 3)
