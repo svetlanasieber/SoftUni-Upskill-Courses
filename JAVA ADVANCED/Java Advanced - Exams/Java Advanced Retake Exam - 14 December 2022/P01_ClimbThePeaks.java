592b0ec53515880442617b7da5029a199a7074ca
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-17 13:22:36 +0200

    Update P01_ClimbThePeaks.java

--- JAVA ADVANCED/Java Advanced - Exams/Java Advanced Retake Exam - 14 December 2022/P01_ClimbThePeaks.java
+++ JAVA ADVANCED/Java Advanced - Exams/Java Advanced Retake Exam - 14 December 2022/P01_ClimbThePeaks.java
@@ -1,8 +1,6 @@
-package _14_Exams._07_JavaAdvancedRetakeExam_14December2022;
-
 import java.util.*;
 
-public class _01_ClimbThePeaks {
+public class P01_ClimbThePeaks {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
