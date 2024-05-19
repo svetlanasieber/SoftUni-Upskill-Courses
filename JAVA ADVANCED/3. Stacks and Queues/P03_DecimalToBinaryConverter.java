3526c02253a586273b3e31143989a62f1be7e074
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-19 15:05:03 +0200

    Update and rename _03_DecimalToBinaryConverter.java to P03_DecimalToBinaryConverter.java

--- JAVA ADVANCED/3. Stacks and Queues/P03_DecimalToBinaryConverter.java
+++ JAVA ADVANCED/3. Stacks and Queues/P03_DecimalToBinaryConverter.java
@@ -1,9 +1,7 @@
-package _01_StacksAndQueues._01_Lab;
-
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class _03_DecimalToBinaryConverter {
+public class P03_DecimalToBinaryConverter {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
