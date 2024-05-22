8b63226cfe8bb6b728d6d5eeeea3c04e0c781c20
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-22 04:35:40 +0200

    Create P08_DivisibleBy3.java

---
+++ JAVA Fundamentals/5. Basic Syntax, Conditional Statements and Loops/P08_DivisibleBy3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P08_DivisibleBy3 {
+    public static void main(String[] args) {
+     
+
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0) {
+             
+                System.out.println(number);
+            }
+        }
+
+    }
+}
