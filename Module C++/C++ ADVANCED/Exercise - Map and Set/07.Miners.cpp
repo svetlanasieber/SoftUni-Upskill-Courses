9ed7fbb8e404ae31804b98c1a3005b5425743c1b
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-23 22:12:11 +0200

    Update 07.Miners.cpp

--- Module C++/C++ ADVANCED/Exercise - Map and Set/07.Miners.cpp
+++ Module C++/C++ ADVANCED/Exercise - Map and Set/07.Miners.cpp
@@ -14,31 +14,31 @@ int main() {
     int quantity;
 
     while (true) {
-        // Четене на име на ресурс
+       
         getline(cin, input);
         if (input == "stop") {
             break;
         }
         resource = input;
 
-        // Четене на количество
+       
         getline(cin, input);
         if (input == "stop") {
             break;
         }
         quantity = stoi(input);
 
-        // Преобразуване на името на ресурса в малки букви за нормализиране
+        
         string resourceLower = resource;
         transform(resourceLower.begin(), resourceLower.end(), resourceLower.begin(), ::tolower);
 
-        // Добавяне или увеличаване на количеството на ресурса в map
+        
         resources[resourceLower] += quantity;
         // Запазване на оригиналното име на ресурса
         originalResources[resourceLower] = resource;
     }
 
-    // Отпечатване на резултата
+  
     for (const auto& pair : resources) {
         cout << originalResources[pair.first] << " -> " << pair.second << endl;
     }
