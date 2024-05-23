a66131fd8aa5f7a186b015d89c4b6d6b7848cd5d
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-23 22:03:13 +0200

    Update 05.SortNumbers.cpp

--- Module C++/C++ ADVANCED/Exercise - Map and Set/05.SortNumbers.cpp
+++ Module C++/C++ ADVANCED/Exercise - Map and Set/05.SortNumbers.cpp
@@ -8,15 +8,15 @@ int main() {
     vector<double> numbers;
     double number;
 
-    // Четене на входни числа
+   
     while (cin >> number) {
         numbers.push_back(number);
     }
 
-    // Сортиране на числата в нарастващ ред
+  
     sort(numbers.begin(), numbers.end());
 
-    // Отпечатване на числата във формат `<=`
+  
     for (size_t i = 0; i < numbers.size(); ++i) {
         if (i > 0) {
             cout << " <= ";
