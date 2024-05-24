4962d60da51df418e96d02190e0ea9df5d7495de
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-25 01:30:57 +0200

    Add files via upload

---
+++ JAVA Basics/7. Conditional Statements Advanced - Lab/P02_WeekendOrWorkingDay.java
@@ -0,0 +1,10 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class P02_WeekendOrWorkingDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
---
+++ JAVA Basics/7. Conditional Statements Advanced - Lab/P03_AnimalType.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class P03_AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
