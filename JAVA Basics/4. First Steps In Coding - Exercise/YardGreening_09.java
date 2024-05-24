d5b51b347371ee061a3974e110437ef1b23dfec5
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-25 01:02:56 +0200

    Update YardGreening_09.java

--- JAVA Basics/4. First Steps In Coding - Exercise/YardGreening_09.java
+++ JAVA Basics/4. First Steps In Coding - Exercise/YardGreening_09.java
@@ -8,7 +8,13 @@ public class YardGreening_09 {
 
         // 1 kvm = 7.61 lv
 
+        double yardMeters = Double.parseDouble(scanner.nextLine());
+        double calcTotal = yardMeters * 7.61;
+        double calcDiscount = calcTotal * 0.18;
+        double total = calcTotal - calcDiscount;
 
+        System.out.println("The final price is: " + total);
+        System.out.println("The discount is: " + calcDiscount);
 
     }
 }
