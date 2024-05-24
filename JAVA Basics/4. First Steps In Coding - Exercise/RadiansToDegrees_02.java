3a1ec56979ec90d76aa27fa37901372a567eac6b
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-25 01:00:44 +0200

    Update RadiansToDegrees_02.java

--- JAVA Basics/4. First Steps In Coding - Exercise/RadiansToDegrees_02.java
+++ JAVA Basics/4. First Steps In Coding - Exercise/RadiansToDegrees_02.java
@@ -6,14 +6,13 @@ public class RadiansToDegrees_02 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        //Math.PI
+        double radians = Double.parseDouble(scanner.nextLine());
 
-
+        double degrees = radians * 180 / Math.PI;
+        System.out.println(degrees);
 
         System.out.println();
-
-
-
-
 
     }
 }
