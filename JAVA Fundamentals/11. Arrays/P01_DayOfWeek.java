43a0c2dc4b418d06534e6d6523f567bfd937abd9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-26 18:56:19 +0200

    Update P01_DayOfWeek.java

--- JAVA Fundamentals/11. Arrays/P01_DayOfWeek.java
+++ JAVA Fundamentals/11. Arrays/P01_DayOfWeek.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P01_Day_of_Week {
+public class P01_DayOfWeek {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
