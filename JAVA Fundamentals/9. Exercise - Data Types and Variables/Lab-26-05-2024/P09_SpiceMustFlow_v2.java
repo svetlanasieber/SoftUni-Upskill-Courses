eac13c8a29545be45abe4548fdac18319f1783c1
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-28 00:30:01 +0200

    Update P09_SpiceMustFlow_v2.java

--- JAVA Fundamentals/9. Exercise - Data Types and Variables/Lab-26-05-2024/P09_SpiceMustFlow_v2.java
+++ JAVA Fundamentals/9. Exercise - Data Types and Variables/Lab-26-05-2024/P09_SpiceMustFlow_v2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SpiceMustFlow_09 {
+public class P09_SpiceMustFlow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
