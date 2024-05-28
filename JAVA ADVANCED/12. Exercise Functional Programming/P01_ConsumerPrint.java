ad8f5732bddf80f767ae6540e90de0b08ef7fd41
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-28 19:16:25 +0200

    Add files via upload

---
+++ JAVA ADVANCED/12. Exercise Functional Programming/P01_ConsumerPrint.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01_ConsumerPrint {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(System.out::println);
+    }
+}
