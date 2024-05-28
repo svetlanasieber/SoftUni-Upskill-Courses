e9bdf4140dbb5ff18f9309f77ec4b057056b3abd
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-28 03:12:04 +0200

    Update P01_RecursiveArraySum.java

--- JAVA ADVANCED/23. Workshop Algorithms Introduction/P01_RecursiveArraySum.java
+++ JAVA ADVANCED/23. Workshop Algorithms Introduction/P01_RecursiveArraySum.java
@@ -19,24 +19,4 @@ public class P01_RecursiveArraySum {
     }
 }
 
-/* import java.util.Arrays;
-import java.util.Scanner;
 
-public class P01_RecursiveArraySum {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int[] array = Arrays.stream(scanner.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        System.out.println(sum(array, 0));
-    }
-
-    private static int sum(int[] array, int index) {
-        if (index == array.length - 1){
-            return array[index];
-        }
-        return array[index] + sum(array, index + 1);
-    }
-} */
