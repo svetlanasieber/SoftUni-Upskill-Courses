d16cb4ad4cbf25a50d2d969a841ec99af8f1cdeb
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-31 16:23:33 +0200

    Create P05.PrintEvenNumbers.cs

---
+++ C# ADVANCED/01. Stacks and Queues Lab/P05.PrintEvenNumbers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Queue<int> numbers = new(
+    Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+while (numbers.Count > 0)
+{
+    int number = numbers.Dequeue();
+
+    if (number % 2 == 0)
+    {
+        Console.Write(number);
+
+        if (numbers.Count > 0)
+        {
+            Console.Write(", ");
+        }
+    }
+}
