0237d91a28bc1a41a07c59556e4bb4599ee1d0b0
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-06-01 01:30:12 +0200

    Create Program.cs

---
+++ PROGRAMMING BASICS WITH CSharp - JAN 2024/C# Fundametals/P02.Passed/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+        if (grade >= 3.00) Console.WriteLine("Passed!");
+    }
+}
