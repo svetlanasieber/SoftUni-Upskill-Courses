08f2975688fc101d33298d183ff7dafa5027d6a6
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-31 16:19:23 +0200

    Create P01.ReverseAString.cs

---
+++ C# ADVANCED/01. Stacks and Queues Lab/P01.ReverseAString.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+string input = Console.ReadLine();
+
+Stack<char> reversed = new();
+
+foreach (char ch in input)
+{
+    reversed.Push(ch);
+}
+
+while (reversed.Count > 0)
+{
+    Console.Write(reversed.Pop());
+}
