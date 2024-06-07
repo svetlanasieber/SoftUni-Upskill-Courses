2a2883e8af3ad5837da656c8e5bcffd0fcd7a6c5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-07 20:56:04 +0200

    Update Program.cs

--- C# Basics/2. First Steps In Coding Exercise/04.VacationBooksList/Program.cs
+++ C# Basics/2. First Steps In Coding Exercise/04.VacationBooksList/Program.cs
@@ -1,1 +1,9 @@
+int pages = int.Parse(Console.ReadLine());
+int pagesPerHours = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
 
+int time = pages / pagesPerHours;
+
+int hoursPerDay = time / days;
+
+Console.WriteLine(hoursPerDay);
