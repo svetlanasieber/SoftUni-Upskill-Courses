1267b36dba7b92950453ca4bc535c7127c1525fd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-07 15:26:57 +0200

    Create P01_MovieProfit.java

---
+++ JAVA Basics/16. Exam Preparation/P01_MovieProfit.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MovieProfit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      
+        String movieName = scanner.nextLine();
+        int countDays = Integer.parseInt(scanner.nextLine());
+        int countTickets = Integer.parseInt(scanner.nextLine());
+        double ticketPrice = Double.parseDouble(scanner.nextLine());
+        int percent = Integer.parseInt(scanner.nextLine());
+
+        double sumForDay = countTickets * ticketPrice;
+        double totalSumWithoutDiscount = sumForDay * countDays;
+        double discount = totalSumWithoutDiscount * percent / 100.0;
+
+        double totalSum = totalSumWithoutDiscount - discount;
+
+        System.out.printf("The profit from the movie %s is %.2f lv.", movieName, totalSum);
+
+
+
+    }
+}
