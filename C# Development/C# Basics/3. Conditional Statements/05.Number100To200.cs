f233a1fa16a6e33f6ea516cebd04dc8e07c63ac9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-09 21:21:20 +0200

    Update 05.Number100To200.cs

--- C# Development/C# Basics/3. Conditional Statements/05.Number100To200.cs
+++ C# Development/C# Basics/3. Conditional Statements/05.Number100To200.cs
@@ -1,1 +1,19 @@
+int number = int.Parse(Console.ReadLine());
+
+
+
+if (number < 100)
+{
+    Console.WriteLine("Less than 100");
+}
+else if (number <= 200)
+{
+    Console.WriteLine("Between 100 and 200");
+}
+else
+{
+    Console.WriteLine("Greater than 200");
+}
+
+
 
