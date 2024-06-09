74d2fdbe7fe21f533056a52103d819c45c8bf810
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-09 21:32:38 +0200

    Update Main.java

--- JAVA ADVANCED/18. Exercise Generics/Exercise-09-06-2024/GenericBox/Main.java
+++ JAVA ADVANCED/18. Exercise Generics/Exercise-09-06-2024/GenericBox/Main.java
@@ -11,46 +11,6 @@ public class Main {
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        // Задача 1:
-//        Box<String> box = new Box<>();
-//        for (int row = 0; row < n; row++) {
-//            String line = scanner.nextLine();
-//            box.add(line);
-//        }
-
-        //Задача 2:
-//        Box<Integer> box = new Box<>();
-//        for (int row = 0; row < n; row++) {
-//            int number = Integer.parseInt(scanner.nextLine());
-//            box.add(number);
-//        }
-
-        //Задача 3:
-//        Box<String> box = new Box<>();
-//        for (int row = 0; row < n; row++) {
-//            String line = scanner.nextLine();
-//            box.add(line);
-//        }
-
-        //Задача 4:
-//        Box<Integer> box = new Box<>();
-//        for (int row = 0; row < n; row++) {
-//            int number = Integer.parseInt(scanner.nextLine());
-//            box.add(number);
-//        }
-//        int index1 = scanner.nextInt();
-//        int index2 = scanner.nextInt();
-//        box.swap(index1, index2);
-
-        //Задача 5:
-//        Box<String> box = new Box<>();
-//        for (int row = 0; row < n; row++) {
-//            String line = scanner.nextLine();
-//            box.add(line);
-//        }
-//        String valueToCompare = scanner.nextLine();
-
-        //Задача 6:
         Box<Double> box = new Box<>();
         for (int row = 0; row < n; row++) {
             double number = Double.parseDouble(scanner.nextLine());
