52c521d6917cb5e0e9c8e369872394dcc82c19d2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-10 17:47:43 +0200

    Delete C# Development directory

--- C# Development/C# Basics/1. First Steps In Coding/First Steps In Coding - Lab/09.YardGreening.cs
+++
@@ -1,8 +0,0 @@
-double area = double.Parse(Console.ReadLine());
-
-double neededPrice = area * 7.61;
-double discount = neededPrice * 0.18;
-double finalPrice = neededPrice - discount;
-
-Console.WriteLine($"The final price is: {finalPrice} lv.");
-Console.WriteLine($"The discount is: {discount} lv.");
--- C# Development/C# Basics/3. Conditional Statements/01.ExcellentResult.cs
+++
@@ -1,6 +0,0 @@
-double grade = double.Parse(Console.ReadLine());
-
-if (grade >= 5.50)
-{
-    Console.WriteLine("Excellent!");
-}
