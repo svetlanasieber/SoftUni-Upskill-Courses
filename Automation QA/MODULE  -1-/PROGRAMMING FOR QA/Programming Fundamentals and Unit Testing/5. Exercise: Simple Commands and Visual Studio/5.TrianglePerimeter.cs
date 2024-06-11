98597a54be23aab5a4afd7ced2ba392c87fde4f1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 19:51:11 +0200

    Update 5.TrianglePerimeter.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/5. Exercise: Simple Commands and Visual Studio/5.TrianglePerimeter.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/5. Exercise: Simple Commands and Visual Studio/5.TrianglePerimeter.cs
@@ -1,1 +1,9 @@
+//The Triangle's perimeter is calculated when you sum all sides values
+//perimeter of triangle = a + b + c
 
+int sideA = int.Parse(Console.ReadLine());
+int sideB = int.Parse(Console.ReadLine());
+int sideC = int.Parse(Console.ReadLine());
+
+int area = sideA + sideB + sideC;
+Console.WriteLine(area);
