ac6143114438384d9d0609ab8ebef4cea046d3d4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 17:13:57 +0200

    Create 2.Greeting.cs

---
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/4. Visual Studio - Lab/2.Greeting.cs
@@ -0,0 +1,11 @@
+namespace _2.Greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
