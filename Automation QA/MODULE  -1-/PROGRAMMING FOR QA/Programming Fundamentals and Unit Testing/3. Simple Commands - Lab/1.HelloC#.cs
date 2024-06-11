e5697a47ef4f7491e904b640d97b73983c165f1d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 16:07:39 +0200

    Update 1.HelloC#.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/1.HelloC#.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/1.HelloC#.cs
@@ -1,1 +1,12 @@
-//https://judge.softuni.org/Contests/Compete/Index/4621#4
+//https://judge.softuni.org/Contests/Compete/Index/4621#0
+
+namespace _1.HelloC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
