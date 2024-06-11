acd73cf7a963a88296d08a0d7d88777fd29029a5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 17:38:23 +0200

    Update 5.TownInfo.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/4. Visual Studio - Lab/5.TownInfo.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/4. Visual Studio - Lab/5.TownInfo.cs
@@ -1,1 +1,12 @@
-
+namespace _5.TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            string country = Console.ReadLine();
+            Console.WriteLine($"{townName} is in {country}");
+        }
+    }
+}
