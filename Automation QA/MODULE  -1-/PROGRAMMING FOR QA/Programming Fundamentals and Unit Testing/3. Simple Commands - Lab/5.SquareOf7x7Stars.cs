f3adad761287323dd87970cc75b2bd5a72b8869b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 16:16:59 +0200

    Update 5.SquareOf7x7Stars.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/5.SquareOf7x7Stars.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/5.SquareOf7x7Stars.cs
@@ -1,1 +1,17 @@
-//https://judge.softuni.org/Contests/Compete/Index/4621#4
+namespace _5.SquareOf7__x_7Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+        }
+    }
+}
+
