0bae8ff1f145ee09bfe617bb9ed696a927500017
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 19:00:01 +0200

    Update 2.SquareArea.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/5. Exercise: Simple Commands and Visual Studio/2.SquareArea.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/5. Exercise: Simple Commands and Visual Studio/2.SquareArea.cs
@@ -1,1 +1,9 @@
+// Square's area is calculated when you multiplied length by length: length * length
+
+int sideA = int.Parse(Console.ReadLine());
+
+
+int area = sideA * sideA;
+
+Console.WriteLine(area);
 
