b4bfb034f04e7223254d3281b6f075ca4401178d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 16:12:45 +0200

    Update 4.Calculations.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/4.Calculations.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/4.Calculations.cs
@@ -1,1 +1,13 @@
-//https://judge.softuni.org/Contests/Compete/Index/4621#4
+namespace _4.Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + (3 * 2));
+            Console.WriteLine(4 * (2 + 3));
+            Console.WriteLine((2 + 5) * (8 - 2) / 7);
+        }
+    }
+}
+
