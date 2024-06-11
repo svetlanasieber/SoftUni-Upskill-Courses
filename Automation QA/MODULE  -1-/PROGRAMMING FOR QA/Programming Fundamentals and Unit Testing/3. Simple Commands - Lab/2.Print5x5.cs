f4fa6cdc156918936b119ffb9f282e1391505988
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 16:08:46 +0200

    Update 2.Print5x5.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/2.Print5x5.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/3. Simple Commands - Lab/2.Print5x5.cs
@@ -1,1 +1,12 @@
 //https://judge.softuni.org/Contests/Compete/Index/4621#4
+
+namespace _2.Print5_x_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 * 5);
+        }
+    }
+}
