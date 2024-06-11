72bbc524a1468e7ec922f91eb0eb5ba19a889ff4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-11 17:08:35 +0200

    Create 00.Demo.cs

---
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/4. Visual Studio - Lab/00.Demo.cs
@@ -0,0 +1,12 @@
+namespace _00.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            Console.WriteLine("Size = " + size);
+            Console.WriteLine("Area = " + size * size);
+        }
+    }
+}
