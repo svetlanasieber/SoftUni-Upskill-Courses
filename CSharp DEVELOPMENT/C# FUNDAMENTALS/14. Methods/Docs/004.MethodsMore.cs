dff780642aa56fa83a33311d866dd8b97ecfd239
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 22:30:54 +0200

    Create 004.MethodsMore.cs

---
+++ CSharp DEVELOPMENT/C# FUNDAMENTALS/14. Methods/Docs/004.MethodsMore.cs
@@ -0,0 +1,28 @@
+
+Console.WriteLine("Hello");
+
+StartEngine();
+Accelerate();
+while (true) 
+{
+    CheckForRain();
+}
+
+// Start Engine
+void StartEngine()
+{
+    for (int i = 0; i < 10; i++)
+    {
+
+    }
+}
+
+void Accelerate()
+{
+    // 30 lines of code
+}
+
+void CheckForRain()
+{
+    // 20 lines of code
+}
