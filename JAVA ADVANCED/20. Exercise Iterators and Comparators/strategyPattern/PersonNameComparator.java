72397738e274872739c68e26f92930a9e07be553
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 11:59:45 +0200

    Update PersonNameComparator.java

--- JAVA ADVANCED/20. Exercise Iterators and Comparators/strategyPattern/PersonNameComparator.java
+++ JAVA ADVANCED/20. Exercise Iterators and Comparators/strategyPattern/PersonNameComparator.java
@@ -8,12 +8,7 @@ public class PersonNameComparator implements Comparator<Person> {
     @Override
     public int compare(Person firstPerson, Person secondPerson) {
 
-        // Начин 1:
-//        Integer firstNameLength = firstPerson.getName().length();
-//        Integer secondNameLength = secondPerson.getName().length();
-//        int nameLengthComparingResult = firstNameLength.compareTo(secondNameLength);
 
-        // Начин 2:
         int nameLengthComparingResult = Integer.compare(firstPerson.getName().length(), secondPerson.getName().length());
 
         if (nameLengthComparingResult == 0) {
