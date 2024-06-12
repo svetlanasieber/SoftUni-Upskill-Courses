598c277f09eec33e7db60c06f4033b4fb902fe2c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 13:17:09 +0200

    Update 03.SumNumbers.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/03.SumNumbers.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/03.SumNumbers.cs
@@ -1,1 +1,12 @@
+int count = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = 1; i <= count; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+    sum += number;
+}
+Console.WriteLine(sum);
+
 
