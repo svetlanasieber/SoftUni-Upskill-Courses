1a00d07daa1d603b185904e51437b7166a114590
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 22:33:44 +0200

    Create 006.MethodsWithParameters.cs

---
+++ CSharp DEVELOPMENT/C# FUNDAMENTALS/14. Methods/Docs/006.MethodsWithParameters.cs
@@ -0,0 +1,10 @@
+string name = "Pesho"; // variable name
+
+PrintName("Dimitrichko");
+PrintName("Unknown");
+
+void PrintName(string name) // "name" is Parameter
+{
+
+    Console.WriteLine($"Hello, my name is {name}");
+}
