430ed6e3e089210990f37ec81b62ca4fe1a8bf99
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 10:47:26 +0200

    Update 00.Demo.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/00.Demo.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/00.Demo.cs
@@ -3,21 +3,20 @@ int a = 1;
 Console.WriteLine(++a);
 Console.WriteLine(a);
 Console.WriteLine("---------------------");
-// Postfix increment
 
+// Postfix increment
 int a2 = 1;
 Console.WriteLine(a2++);
 Console.WriteLine(a2);
 Console.WriteLine("---------------------");
-// Prefix decrement
 
+// Prefix decrement
 int b = 1;
 Console.WriteLine(--b);
 Console.WriteLine(b);
 Console.WriteLine("---------------------");
 
 // Postfix decrement
-
 int b2 = 1;
 Console.WriteLine(b2--);
 Console.WriteLine(b2);
