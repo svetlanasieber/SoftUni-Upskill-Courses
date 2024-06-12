9ddb619381a4c085faecfee6c7a8869e6713a639
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 12:41:13 +0200

    Update 02.FirstNNumbersSum.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/02.FirstNNumbersSum.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/02.FirstNNumbersSum.cs
@@ -1,3 +1,25 @@
 //-> https://judge.softuni.org/Contests/Compete/Index/4404#1
 
+//which sums the numbers 1…n:
+
+//1. Reads an integer number n from the console
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+//2. Sums all numbers from 1 to n
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i; //sum += i;
+}
+
+//3. Prints the numbers and the sum on the console as shown below
+// 1+; 2+, 3+, 4+, 5+ = 15
+for (int i = 1; i < number; i++)
+{
+    Console.Write($"{i}+");
+}
+Console.Write(number);
+Console.WriteLine($"={sum}");
+
 
