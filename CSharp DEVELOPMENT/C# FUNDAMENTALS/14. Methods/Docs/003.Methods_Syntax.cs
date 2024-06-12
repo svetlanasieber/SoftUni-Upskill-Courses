8947a9a3d41bb5e6b5ba87e5d64d51b547079935
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 22:30:15 +0200

    Create 003.Methods_Syntax.cs

---
+++ CSharp DEVELOPMENT/C# FUNDAMENTALS/14. Methods/Docs/003.Methods_Syntax.cs
@@ -0,0 +1,21 @@
+
+Console.WriteLine("");
+
+//int[] array;
+
+//Array.Sort();
+
+Greeting();//calling Method
+
+Greeting();
+
+Greeting();
+
+
+//type //name //()
+void Greeting()
+{
+    //method body
+    Console.WriteLine("Real greeting");
+  
+}
