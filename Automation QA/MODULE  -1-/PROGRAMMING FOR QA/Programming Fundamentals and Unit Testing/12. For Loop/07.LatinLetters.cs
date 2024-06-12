849565589c20cd197115b81b010ea4f08e46286b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 15:05:48 +0200

    Update 07.LatinLetters.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/07.LatinLetters.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/12. For Loop/07.LatinLetters.cs
@@ -1,1 +1,8 @@
+char startLetter = char.Parse(Console.ReadLine());
+char endLetter = char.Parse(Console.ReadLine());
+
+for (char i = startLetter; i <= endLetter; i++)
+{
+    Console.Write(i + " ");
+}
 
