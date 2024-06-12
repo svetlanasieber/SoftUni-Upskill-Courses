c3665e3c57bcd1d260893065c59f6e6ec0b2caac
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-12 22:29:27 +0200

    Create 001.Methods.cs

---
+++ CSharp DEVELOPMENT/C# FUNDAMENTALS/14. Methods/Docs/001.Methods.cs
@@ -0,0 +1,20 @@
+// Parameters
+
+Greeting("Dimitrichko");
+
+Console.WriteLine("Greeting again:");
+
+Greeting("Goshko");
+
+for (int i = 0; i < 4; i++)
+{
+    Greeting("Viktor");
+}
+ 
+void Greeting(string name)
+{
+    Console.WriteLine("Hello");
+    Console.WriteLine($"My name is {name}");
+    Console.WriteLine("And I study in SoftUni");
+
+}
