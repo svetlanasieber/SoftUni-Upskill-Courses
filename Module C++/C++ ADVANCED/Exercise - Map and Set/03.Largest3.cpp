8a1a293bea67f26cae5d2294e56de5ad8e4574df
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-16 06:53:30 +0200

    Update 03.Largest3.cpp

--- Module C++/C++ ADVANCED/Exercise - Map and Set/03.Largest3.cpp
+++ Module C++/C++ ADVANCED/Exercise - Map and Set/03.Largest3.cpp
@@ -8,15 +8,12 @@ int main() {
     vector<double> numbers;
     double number;
 
-    // Read input numbers
     while (cin >> number) {
         numbers.push_back(number);
     }
 
-    // Sort the numbers in descending order
     sort(numbers.rbegin(), numbers.rend());
 
-    // Print the largest 3 numbers or all if there are fewer than 3
     int count = min(3, (int)numbers.size());
     for (int i = 0; i < count; i++) {
         cout << numbers[i] << " ";
