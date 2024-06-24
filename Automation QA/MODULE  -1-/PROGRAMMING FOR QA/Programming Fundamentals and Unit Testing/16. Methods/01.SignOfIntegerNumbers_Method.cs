19776a10b99a1bae8b3c94606ad3ec86078edb39
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-24 22:54:51 +0200

    Create 01.SignOfIntegerNumbers_Method.cs

---
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/16. Methods/01.SignOfIntegerNumbers_Method.cs
@@ -0,0 +1,20 @@
+int n = int.Parse(Console.ReadLine());
+
+PrintSign(n);
+
+static void PrintSign(int n)
+{
+
+    if (n > 0)
+    {
+        Console.WriteLine($"The number {n} is positive.");
+    }
+    else if (n < 0)
+    {
+        Console.WriteLine($"The number {n} is negative.");
+    }
+    else
+    {
+        Console.WriteLine($"The number {n} is zero.");
+    }
+}
