5d60af7689f4d0a32f1a42fd7b57721eca677ec5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-24 23:08:56 +0200

    Update 02.Grades.cs

--- Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/16. Methods/02.Grades.cs
+++ Automation QA/MODULE  -1-/PROGRAMMING FOR QA/Programming Fundamentals and Unit Testing/16. Methods/02.Grades.cs
@@ -1,1 +1,30 @@
+double grade = double.Parse(Console.ReadLine());
+
+
+PrintGradeInWords(grade);
+
+static void PrintGradeInWords(double grade)
+{
+    if (grade < 3.00)
+    {
+        Console.WriteLine("Fail");
+        
+    }
+    else if (grade < 3.50)
+    {
+        Console.WriteLine("Average");
+    }
+    else if (grade < 4.50)
+    {
+        Console.WriteLine("Good");
+    }
+    else if (grade < 5.50)
+    {
+        Console.WriteLine("Very good");
+    }
+    else
+    {
+        Console.WriteLine("Excellent");
+    }
+}
 
