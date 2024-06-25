9f47a21217e391f74054c67545b701febec4c152
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-25 02:52:47 +0200

    Delete Module C++/C++ Fundamentals directory

--- Module C++/C++ Fundamentals/3. Basic Syntax Lab/01.Declaring-Initializing-Variables.cpp
+++
@@ -1,8 +0,0 @@
-int numGlobal;
-
-int main() {
-    int num;
-    num = 5;
-    int sameNum(5);
-    return 0;
-}
--- Module C++/C++ Fundamentals/3. Basic Syntax Lab/02.Uninitialized-Locals.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-
-int main() {
-    int georgeApples;
-    std::cout << georgeApples << std::endl;
-    return 0;
-}
