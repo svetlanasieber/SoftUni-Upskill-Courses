ca5f0df054a70a0871c6ad3cd40650f0fda40761
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-26 23:23:48 +0200

    Create P02.CalculateSpeed.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/6. Data Types and Variables/P02.CalculateSpeed.cs
@@ -0,0 +1,8 @@
+//speed = distance / time
+
+double distance = double.Parse(Console.ReadLine());
+double time = double.Parse(Console.ReadLine());
+
+double speed = distance / time;
+
+Console.WriteLine($"{speed:F2}");
