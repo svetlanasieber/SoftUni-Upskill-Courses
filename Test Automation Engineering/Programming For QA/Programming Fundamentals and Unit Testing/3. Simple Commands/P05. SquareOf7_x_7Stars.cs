1c9f15ec55a45274b7003564538fbdd6a97f2514
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-26 23:10:29 +0200

    Create P05. SquareOf7_x_7Stars.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/3. Simple Commands/P05. SquareOf7_x_7Stars.cs
@@ -0,0 +1,16 @@
+namespace P05.SquareOf7_x_7Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+            Console.WriteLine("* * * * * * *");
+        }
+    }
+}
