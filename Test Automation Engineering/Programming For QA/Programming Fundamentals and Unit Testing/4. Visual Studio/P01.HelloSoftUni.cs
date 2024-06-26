23dd72ed7258dfa2529ed7624af65e09353c9ff2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-26 23:12:11 +0200

    Create P01.HelloSoftUni.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/4. Visual Studio/P01.HelloSoftUni.cs
@@ -0,0 +1,10 @@
+namespace P01.HelloSoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SoftUni");
+        }
+    }
+}
