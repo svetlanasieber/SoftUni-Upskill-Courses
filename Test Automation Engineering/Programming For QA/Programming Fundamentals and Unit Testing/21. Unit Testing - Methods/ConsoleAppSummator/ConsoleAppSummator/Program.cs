5485a4c2b740c1e62267d502a8a186dcde21dc4a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-27 16:48:46 +0200

    Add files via upload

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/21. Unit Testing - Methods/ConsoleAppSummator/ConsoleAppSummator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleAppSummator
+{
+    public class Program //iternal smeniame na public
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public static int Sum(int[] nums)
+        {
+            int result = 0;
+
+            foreach (int num in nums)
+            {
+                result += num;
+            }
+
+            return result;
+        }
+
+
+
+
+
+
+
+
+    }
+}
