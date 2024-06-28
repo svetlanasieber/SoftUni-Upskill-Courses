f3f45238fe41d0f998d99adb220ff48d3ac4a11a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-28 16:44:50 +0200

    Create P07.LatinLetters.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/12. For Loop/P07.LatinLetters.cs
@@ -0,0 +1,7 @@
+char startLetter = char.Parse(Console.ReadLine());
+char endLetter = char.Parse(Console.ReadLine());
+
+for (char i = startLetter; i <= endLetter; i++)
+{
+    Console.Write(i + " ");
+}
