3b31805fbb8bf165e845c8703ce0898030089f72
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-28 22:47:26 +0200

    Create P01_PermutationsWithoutRepetition.java

---
+++ Algorithms Fundamentals with JAVA/Combinatorial Problems - Lab/P01_PermutationsWithoutRepetition.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class PermutationsWithoutRepetitions {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] elements = sc.nextLine().split(" ");
+
+		permute(elements, 0);
+
+		sc.close();
+
+	}
+
+	private static void permute(String[] elements, int index) {
+		if (index == elements.length) {
+			print(elements);
+			return;
+		}
+		permute(elements, index + 1);
+
+		for (int i = index + 1; i < elements.length; i++) {
+			swap(elements, index, i);
+			permute(elements, index + 1);
+			swap(elements, index, i);
+		}
+	}
+
+	private static void swap(String[] arr, int first, int second) {
+		String temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
+
+	}
+
+	private static void print(String[] permutes) {
+		System.out.println(String.join(" ", permutes));
+
+	}
+
+}
