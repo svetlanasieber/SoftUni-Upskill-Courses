60596e202df7cbdfef5502995122d4c2094a227a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-01 01:36:06 +0200

    Update 02.MiddleElements.cs

--- Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/24. Exam Preparation/Lab-1-06-2024/02.MiddleElements.cs
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/24. Exam Preparation/Lab-1-06-2024/02.MiddleElements.cs
@@ -1,1 +1,10 @@
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+int middle = numbers.Length / 2;
+
+int firstNumber = numbers[middle - 1];
+int secondNumber = numbers[middle];
+
+double average = (firstNumber + secondNumber) / 2.0;
+
+Console.WriteLine($"{average:F2}");
