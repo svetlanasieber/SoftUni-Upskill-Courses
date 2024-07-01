ee7c189a4b019a497ec6333184263e0ec901fec9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-01 22:43:19 +0200

    Update FoldSumTests.cs

--- Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/24. Exam Preparation/Lab-1-06-2024/UnitTestingFoldArray/TestApp.Tests/FoldSumTests.cs
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/24. Exam Preparation/Lab-1-06-2024/UnitTestingFoldArray/TestApp.Tests/FoldSumTests.cs
@@ -1,17 +1,22 @@
-using System;
 using NUnit.Framework;
 
 namespace TestApp.Tests;
 
 public class FoldSumTests
 {
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, "7 7 7 19 19 19")]
+    [TestCase(new int[] { 1, 1, 1, 1,  }, "2 2")]
+    [TestCase(new int[] { 1, -2, 3, -4, 5, -6, 7, -8 }, "1 -3 -3 1")]
+    [TestCase(new int[] { -1, -2, -3, -4 }, "-3 -7")]
     public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
     {
-        // TODO: implement the test and finish the test cases
+        //Arrange
+
+        //Act
+        string actual = FoldSum.FoldArray(arr);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
     }
 }
