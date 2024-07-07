049049d97265233249341690b5c62bd0f981e734
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-07 16:46:16 +0200

    Add files via upload

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/27. Regular Exam/04.Vowels Counter/VowelsCounter/TestApp/VowelsCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp;
+
+public class VowelsCounter
+{
+    public static int CountTotalVowels(List<string> words)
+    {
+        int totalVowels = 0;
+
+        string allChars = string.Join(string.Empty, words);
+
+        foreach (char singleChar in allChars.ToLower())
+        {
+            if ("aeiou".Contains(singleChar))
+            {
+                totalVowels += 1;
+            }
+        }
+
+        return totalVowels;
+    }
+}
+
+
