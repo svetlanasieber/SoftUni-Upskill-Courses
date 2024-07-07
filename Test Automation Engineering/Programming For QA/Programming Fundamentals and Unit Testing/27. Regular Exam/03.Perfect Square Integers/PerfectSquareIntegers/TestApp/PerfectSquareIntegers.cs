868194be70923331386ab6f7465598a32c90c37f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-07 16:43:24 +0200

    Add files via upload

---
+++ Test Automation Engineering/Programming For QA/Programming Fundamentals and Unit Testing/27. Regular Exam/03.Perfect Square Integers/PerfectSquareIntegers/TestApp/PerfectSquareIntegers.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace TestApp;
+
+public class PerfectSquareIntegers
+{
+    public static string FindPerfectSquares(int start, int end)
+    {
+        if (start > end)
+        {
+            return "Start number should be less than end number.";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int num = start; num <= end; num++)
+        {
+            if (IsPerfectSquare(num))
+            {
+                result.Append(num + " ");
+            }
+        }
+
+        return result.ToString().Trim();
+    }
+
+    private static bool IsPerfectSquare(int number)
+    {
+        if (number < 0)
+        {
+            return false;
+        }
+
+        int sqrt = (int)Math.Sqrt(number);
+        return sqrt * sqrt == number;
+    }
+}
+
