468e7f20a8f530b4d4e23e615b8c71dfb27b8019
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-14 20:44:04 +0200

    Update 02.Repeat Strings.cs

--- C# Fundamentals/28. Text Processing/02.Repeat Strings.cs
+++ C# Fundamentals/28. Text Processing/02.Repeat Strings.cs
@@ -7,3 +7,16 @@ foreach (string word in words)
         result += word;
 }
 Console.WriteLine(result);
+
+
+//v2
+/*
+string[] words = Console.ReadLine().Split(" ");
+for (int i = 0; i < words.Length; i++)
+{
+    for (int j = 0; j < words[i].Length; j++)
+    {
+        Console.Write(words[i]);
+    }
+}
+*/
