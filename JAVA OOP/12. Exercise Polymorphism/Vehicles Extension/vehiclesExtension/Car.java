4cc86abb166f36e88bec72ca82f0ef40203c06a4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-14 14:05:14 +0200

    Add files via upload

---
+++ JAVA OOP/12. Exercise Polymorphism/Vehicles Extension/vehiclesExtension/Car.java
@@ -0,0 +1,8 @@
+package vehiclesExtension;
+
+public class Car extends AbstractVehicle {
+    private static final double SUMMER_FUEL_CONSUMPTION = 0.9;
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption + SUMMER_FUEL_CONSUMPTION,  tankCapacity);
+    }
+}
