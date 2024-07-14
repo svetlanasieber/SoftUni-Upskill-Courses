bc485408c2697db73f9ff9c9794eb6d1e7094148
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-14 20:37:45 +0200

    Create 02.Repeat Strings.cs

---
+++ C# Fundamentals/28. Text Processing/02.Repeat Strings.cs
@@ -0,0 +1,9 @@
+string[] words = Console.ReadLine().Split();
+string result = "";
+foreach (string word in words)
+{
+    int repeatTimes = word.Length;
+    for (int i = 0; i < repeatTimes; i++)
+        result += word;
+}
+Console.WriteLine(result);
