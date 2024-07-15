01de577bb718d2ac77a9c96bae6fe4f28db80777
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-15 20:30:57 +0200

    Update P04_WordFilter_2.java

--- JAVA Fundamentals/25. Associative Arrays/Maps, Lambda and Stream API - Lab/P04_WordFilter_2.java
+++ JAVA Fundamentals/25. Associative Arrays/Maps, Lambda and Stream API - Lab/P04_WordFilter_2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class WordFilter_04 {
+public class P04_WordFilter_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] words = Arrays.stream(scanner.nextLine().split(" "))
