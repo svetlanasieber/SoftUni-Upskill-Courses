80213194971b70bcf822d9a5067d26cfcf7f1026
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-16 15:42:43 +0200

    Update 01.CountRealNumbers.cs

--- Test Automation Engineering/Programming For QA/Programming Advanced for QA/6. Dictionaries, Lambda and LINQ/01.CountRealNumbers.cs
+++ Test Automation Engineering/Programming For QA/Programming Advanced for QA/6. Dictionaries, Lambda and LINQ/01.CountRealNumbers.cs
@@ -1,4 +1,37 @@
-double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+double [] numbers = Console.ReadLine()      
+                    .Split(" ")            
+                    .Select(double.Parse)   
+                    .ToArray();
+
+
+SortedDictionary<double, int> countNumbers = new SortedDictionary<double, int>();
+
+foreach(double number in numbers)
+{
+   
+    if (countNumbers.ContainsKey(number))
+    {
+        countNumbers[number]++;
+    }
+    else
+    {
+        countNumbers.Add(number, 1);
+    }
+}
+
+foreach (KeyValuePair<double, int> entry in countNumbers)
+{
+  
+    Console.WriteLine(entry.Key + " -> " + entry.Value);
+}
+
+
+
+
+
+
+
+/*double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
 var counts = new SortedDictionary<double, int>();
 foreach (var num in nums)
     if (counts.ContainsKey(num))
@@ -7,3 +40,4 @@ foreach (var num in nums)
         counts[num] = 1;
 foreach (var num in counts)
     Console.WriteLine($"{num.Key} -> {num.Value}");
+*/
