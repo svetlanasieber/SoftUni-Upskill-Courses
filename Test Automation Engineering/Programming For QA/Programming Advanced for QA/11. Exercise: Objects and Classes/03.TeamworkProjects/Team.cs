11f9fd558abd6a628ecfc7246a95da27ada06170
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-07-30 11:43:10 +0200

    Create Team.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Advanced for QA/11. Exercise: Objects and Classes/03.TeamworkProjects/Team.cs
@@ -0,0 +1,17 @@
+namespace _03.TeamworkProjects
+{
+	public class Team
+	{
+		public Team(string name, string creator)
+		{
+			Name = name;
+			Creator = creator;
+			Members = new List<string> { };
+		}
+
+		public string Name { get; set; }
+
+		public string Creator { get; set; }
+
+		public List<string> Members { get; set; } 
+	}
