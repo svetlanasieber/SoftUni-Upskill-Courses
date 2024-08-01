1381e840cac49bae542f785c19397efb3e4282fb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-01 21:30:17 +0200

    Create Tire.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Advanced for QA/10. Definning Classes/CarEngineAndTires/Tire.cs
@@ -0,0 +1,18 @@
+namespace CarManufacturer
+{
+    public class Tire
+    {
+       
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+       
+        public Tire(int year, double pressure)
+        {
+            
+            Year = year;
+            Pressure = pressure;
+        }
+    }
+}
+
