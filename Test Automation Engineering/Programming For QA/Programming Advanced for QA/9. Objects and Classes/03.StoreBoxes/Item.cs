2beb7c9b518b0613370e32447084a2e8bb0818dd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-01 21:20:02 +0200

    Create Item.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Advanced for QA/9. Objects and Classes/03.StoreBoxes/Item.cs
@@ -0,0 +1,14 @@
+public class Item
+{
+   
+     public string Name { get; set; }
+     public double Price { get; set; }
+
+    public Item (string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+}
+
