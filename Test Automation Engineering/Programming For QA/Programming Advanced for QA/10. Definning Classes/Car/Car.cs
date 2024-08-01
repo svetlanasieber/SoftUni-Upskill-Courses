3b1c917cac2ce7889bfe493221e3a4fc53173cd5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-01 21:22:11 +0200

    Create Car.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Advanced for QA/10. Definning Classes/Car/Car.cs
@@ -0,0 +1,20 @@
+namespace CarManufacturer
+{
+    public class Car
+    {
+       
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Car()
+        {
+            
+            //Make = null
+            //Model = null
+            //Year = 0
+        }
+
+    }
+}
+
