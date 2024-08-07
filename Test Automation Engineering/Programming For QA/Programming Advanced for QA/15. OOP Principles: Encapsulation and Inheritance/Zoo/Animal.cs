81e0492b10fdbf34ab263783cce781987f1f40b8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-07 10:50:58 +0200

    Create Animal.cs

---
+++ Test Automation Engineering/Programming For QA/Programming Advanced for QA/15. OOP Principles: Encapsulation and Inheritance/Zoo/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace Zoo;
+
+
+public class Animal
+{
+
+    private string name;
+
+    public string Name { get; set; }
+
+    public Animal(string name)
+    {
+      
+        this.Name = name;
+    }
+
+
+}
