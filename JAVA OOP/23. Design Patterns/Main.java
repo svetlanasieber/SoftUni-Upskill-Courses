ee5aa80b595aee3363a8d8b6298e9ac4641eec0c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-13 21:30:57 +0200

    Create Main.java

---
+++ JAVA OOP/23. Design Patterns/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Database mysql = Database.getInstance("MySQL");
+        Database mariaDB = Database.getInstance("MariaDB");
+        System.out.println();
+
+    }
+}
