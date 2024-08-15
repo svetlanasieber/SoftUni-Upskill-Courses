206a095a5f78ff52beea7ba831efe4da90dd4589
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-16 00:41:32 +0200

    Create P00_Demo.java

---
+++ JAVA Fundamentals/11. Arrays/Demo/P00_Demo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P00_Demo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] array = new int[10];
+
+        System.out.println(array.length);
+
+        array[0] = 4;
+        array[1] = 6;
+        array[2] = 9;
+
+        System.out.println(array[2]);
+        System.out.println(array[0]);
+        System.out.println(array[array.length - 1]);
+    }
+}
