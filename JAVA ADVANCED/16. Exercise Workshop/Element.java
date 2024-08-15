ee1c64f8d834dbfcd8b5dd98e1a431685592f5d1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-16 00:02:25 +0200

    Update Element.java

--- JAVA ADVANCED/16. Exercise Workshop/Element.java
+++ JAVA ADVANCED/16. Exercise Workshop/Element.java
@@ -1,1 +1,18 @@
 
+public class Element {
+
+    int value;
+    Element prev;
+    Element next;
+
+    public Element(int value, Element prev, Element next) {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    
+    public String toString() {
+        return String.valueOf(this.value);
+    }
+}
