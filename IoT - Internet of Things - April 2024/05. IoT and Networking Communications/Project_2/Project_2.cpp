f3c945887cadd593c10f91190c169d6a80556871
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-19 01:32:31 +0300

    Update Project_2.cpp

--- IoT - Internet of Things - April 2024/05. IoT and Networking Communications/Project_2/Project_2.cpp
+++ IoT - Internet of Things - April 2024/05. IoT and Networking Communications/Project_2/Project_2.cpp
@@ -1,19 +1,17 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-// Replace with your network credentials
+
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-WebServer server(80);  // Create a web server object that listens for HTTP request on port 80
+WebServer server(80);  
 
-// This function handles all incoming requests
 void handleAllRequests() {
   String message = "URI Requested: ";
   message += server.uri();  // Get the requested URI
 
-  // You can implement different behaviors based on the URI
-  // For example:
+
   if (server.uri() == "/") {
     server.send(200, "text/html", "<h1>Welcome to the ESP32 Web Server</h1>");
   } else if (server.uri() == "/info") {
@@ -26,9 +24,9 @@ void handleAllRequests() {
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);  // Connect to your WiFi network
+  WiFi.begin(ssid, password); 
 
-  while (WiFi.status() != WL_CONNECTED) {  // Wait until the connection has been established
+  while (WiFi.status() != WL_CONNECTED) {  /
     delay(500);
     Serial.print(".");
   }
@@ -38,12 +36,12 @@ void setup() {
   Serial.print(WiFi.localIP());
   Serial.println("/");
 
-  // Setup a catch-all route
+
   server.onNotFound(handleAllRequests);
 
-  server.begin();  // Start the server
+  server.begin();  
 }
 
 void loop() {
-  server.handleClient();  // Handle client requests
+  server.handleClient();  
 }
