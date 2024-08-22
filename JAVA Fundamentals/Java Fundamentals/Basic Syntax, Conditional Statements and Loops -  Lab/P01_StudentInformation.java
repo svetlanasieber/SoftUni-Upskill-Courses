4358cbb5ec7d64b0e12ddaa50fb9588f7d83833c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-22 19:13:23 +0300

    Update P01_StudentInformation.java

--- JAVA Fundamentals/Java Fundamentals/Basic Syntax, Conditional Statements and Loops -  Lab/P01_StudentInformation.java
+++ JAVA Fundamentals/Java Fundamentals/Basic Syntax, Conditional Statements and Loops -  Lab/P01_StudentInformation.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class P01_StudentInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double averageGrade = Double.parseDouble(scanner.nextLine());
+
+
+//"Name: {student name}, Age: {student age}, Grade: {student grade}".
+        System.out.printf("Name: %s, Age: %s, Grade: %.2f", name, age, averageGrade);
+    }
+}
