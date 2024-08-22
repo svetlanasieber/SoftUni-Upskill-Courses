d77de5950601db082f31daf7642537976c4bd432
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-22 17:59:38 +0300

    Create Demo_Printf.java

---
+++ JAVA Fundamentals/Java Fundamentals/Basic Syntax, Conditional Statements and Loops -  Lab/Demo_Printf.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Demo_Printf {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = 25;
+        String name = "Desislava";
+        System.out.printf("I am %s: %d years old.\n", name, age);
