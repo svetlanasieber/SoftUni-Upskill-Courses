dad98126a810ea4616aa374ab1c1506fe94049f6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-24 20:31:39 +0300

    Create Demo1.java

---
+++ JAVA Fundamentals/Java Fundamentals/Arrays - Lab/Demo/Demo1.java
@@ -0,0 +1,21 @@
+public class Demo1 {
+    public static void main(String[] args) {
+
+     
+        int[] numbers = new int[4];
+
+  
+        numbers[0] = 22;
+        numbers[1] = 13;
+        numbers[2] = 6;
+        numbers[3] = 100;
+
+        System.out.println(numbers.length);
+        System.out.println(numbers[numbers.length - 1]); 
+
+
+
+
+
+    }
+}
