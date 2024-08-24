5ae99aab6c8bf7f438ee41b2192a0a56e91c2739
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-24 20:37:59 +0300

    Create P02_PrintNumbersInReverseOrder.java

---
+++ JAVA Fundamentals/Java Fundamentals/Arrays - Lab/02.PrintNumbersInReverseOrder/P02_PrintNumbersInReverseOrder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p02_PrintNumbersInReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+      
+    }
+}
