4def70a08005aa10a174604e4101ebd0341d954f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-24 20:43:21 +0300

    Create Demo_Letters.java

---
+++ JAVA Fundamentals/Java Fundamentals/Arrays - Lab/Demo/Demo_Letters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Demo_Letters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "Peter"; // name: "Peter"
+        char[] letters = name.toCharArray(); // letters: [P, e, t, e, r] name: "Peter"
+        System.out.printf("");
+    }
+}
