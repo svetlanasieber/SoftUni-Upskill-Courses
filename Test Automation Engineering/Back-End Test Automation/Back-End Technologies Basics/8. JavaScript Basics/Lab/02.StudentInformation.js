70ba98045a5b460d369f1d279731330c5cbe2552
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-09-13 01:22:58 +0200

    Update and rename 02.StudentInformation.ja to 02.StudentInformation.js

--- Test Automation Engineering/Back-End Test Automation/Back-End Technologies Basics/8. JavaScript Basics/Lab/02.StudentInformation.js
+++ Test Automation Engineering/Back-End Test Automation/Back-End Technologies Basics/8. JavaScript Basics/Lab/02.StudentInformation.js
@@ -4,4 +4,4 @@ function solve(name, age, grade) {
     console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
 }
 
-solve('Pesho', 20, 5.55555555555555555)
+solve('Pesho', 20, 5.55555555555555555);
