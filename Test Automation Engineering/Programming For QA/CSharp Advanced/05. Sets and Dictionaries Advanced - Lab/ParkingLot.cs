cd099afa1c53ade47a03b02aa53da35ab527bd97
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-09-17 00:28:26 +0200

    Create ParkingLot.cs

---
+++ Test Automation Engineering/Programming For QA/CSharp Advanced/05. Sets and Dictionaries Advanced - Lab/ParkingLot.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    internal class P07ParkingLot
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            HashSet<string> set = new HashSet<string>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string direction = input.Split(", ")[0];
+                string carNumber = input.Split(", ")[1];
+                if (direction == "IN")
+                    set.Add(carNumber);
+                if (direction == "OUT")
+                    set.Remove(carNumber);
+
+            }
+            if (set.Count != 0)
+            {
+                foreach (var item in set)
+                    Console.WriteLine(item);
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
