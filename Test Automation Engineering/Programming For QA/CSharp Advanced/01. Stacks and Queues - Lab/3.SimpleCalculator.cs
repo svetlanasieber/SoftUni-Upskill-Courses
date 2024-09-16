c450c261840226dd9d92886bc9de7fde01745f63
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-09-16 23:53:05 +0200

    Create 3.SimpleCalculator.cs

---
+++ Test Automation Engineering/Programming For QA/CSharp Advanced/01. Stacks and Queues - Lab/3.SimpleCalculator.cs
@@ -0,0 +1,21 @@
+Stack<string> tokens = new(Console.ReadLine()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Reverse());
+
+int result = int.Parse(tokens.Pop());
+
+while (tokens.Count > 0)
+{
+    string operation = tokens.Pop();
+
+    if (operation == "+")
+    {
+        result += int.Parse(tokens.Pop());
+    }
+    else if (operation == "-")
+    {
+        result -= int.Parse(tokens.Pop());
+    }
+}
+
+Console.WriteLine(result);
